daefc335751b04ac2c3cba403b1d7ac584c43ece
Toby Liu <ybot1122@gmail.com>
2016-06-25 23:29:26 -0700

    lets get some user value

--- basicSkills.js
+++ basicSkills.js
@@ -1,21 +1,49 @@
 
+/**
+    Receives an Alexa request
+    Diffrentiates between Launch and Intent
+    Writes to SQS
+    and responds with a speechlet
+**/
+
 'use strict';
 
+var AWS = require("aws-sdk");
+AWS.config.region = 'us-east-1';
+var sqsURL = 'https://sqs.us-east-1.amazonaws.com/365496274414/colorexpert';
 exports.handler = function (event, context) {
     try {
         console.log("event.session.application.applicationId=" + event.session.application.applicationId);
         var speechletResponse;
         if (event.request.type === "LaunchRequest") {
             speechletResponse = buildSpeechletResponse("hey I am the launch");
-            context.succeed(buildResponse(speechletResponse));
+            writeToSqs('launch', context, speechletResponse);
         } else if (event.request.type === "IntentRequest") {
             speechletResponse = buildSpeechletResponse("hey I am the intent");
-            context.succeed(buildResponse(speechletResponse));
+            writeToSqs('launch', context, speechletResponse);
         }
     } catch (e) {
         context.fail("Exception: " + e);
     }
 };
+
+function writeToSqs(msg, context, speechletResponse) {
+    var queue = new AWS.SQS();
+    var params = {
+        MessageBody: "message is " + msg,
+        QueueUrl: sqsURL
+    };
+    
+    console.log('ooooga we writin to ' + sqsURL);
+    queue.sendMessage(params, function (err, data) {
+        if (err) {
+            console.log(err, err.stack);
+        } else {
+            console.log("message Sent");
+        }
+        context.succeed(buildResponse(speechletResponse));
+    });
+}
 
 function buildSpeechletResponse(output) {
     return {
