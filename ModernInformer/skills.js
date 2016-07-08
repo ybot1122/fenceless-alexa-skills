787433d6d1d3cd2ec33f2f2d08459b4c5ff3c8b0
Toby Liu <ybot1122@gmail.com>
2016-07-08 07:04:50 -0700

    2 space tabbing

--- ModernInformer/skills.js
+++ ModernInformer/skills.js
@@ -11,57 +11,58 @@ var sqsURL = 'https://sqs.us-east-1.amazonaws.com/365496274414/colorexpert';
 var appName = 'Headline Hitter';
 var http = require('http');
 exports.handler = function (event, context) {
-    try {
-        var messAttrs = {};
-        messAttrs.app = {
-            DataType: 'String',
-            StringValue: appName
-        };
-        if (event.request.type === "LaunchRequest") {
-            var speechletResponse = buildSpeechletResponse("Top Story");
-            messAttrs.content = {
-                DataType: 'String',
-                StringValue: 'top'
-            };
-            writeToSqs(appName + '::' + 'Top Story', messAttrs);
-            context.succeed(buildResponse(speechletResponse));
-        } else if (event.request.type === "IntentRequest") {
-            var intentName = event.request.intent.name;
-            var suffix;
-            var topic;
-            messAttrs.content = {
-                DataType: 'String'
-            };
-            switch(intentName) {
-                case "TopHeadline":
-                    suffix = 'Top Story';
-                    messAttrs.content.StringValue = 'top';
-                    topic = 'home';
-                    break;
-                case "SportsHeadline":
-                    suffix = 'Top Sports Story';
-                    messAttrs.content.StringValue = 'sports';
-                    topic = 'sports';
-                    break;
-                case "PoliticsHeadline":
-                    suffix = 'Top Politics Story';
-                    messAttrs.content.StringValue = 'politics';
-                    topic = 'politics';
-                    break;
-                case "WorldHeadline":
-                    suffix = 'Top World Story';
-                    messAttrs.content.StringValue = 'world';
-                    topic = 'world';
-                    break;
-                default:
-                    throw "unspecified intent";
-            }
-            writeToSqs(appName + '::' + suffix, messAttrs);
-            requestHeadline(context, topic);
-        }
-    } catch (e) {
-        context.fail("Exception: " + e);
+  try {
+    var messAttrs = {};
+    messAttrs.app = {
+      DataType: 'String',
+      StringValue: appName
+    };
+
+    if (event.request.type === "LaunchRequest") {
+      var speechletResponse = buildSpeechletResponse("Top Story");
+      messAttrs.content = {
+        DataType: 'String',
+        StringValue: 'top'
+      };
+      writeToSqs(appName + '::' + 'Top Story', messAttrs);
+      context.succeed(buildResponse(speechletResponse));
+    } else if (event.request.type === "IntentRequest") {
+      var intentName = event.request.intent.name;
+      var suffix;
+      var topic;
+      messAttrs.content = {
+        DataType: 'String'
+      };
+      switch(intentName) {
+        case "TopHeadline":
+          suffix = 'Top Story';
+          messAttrs.content.StringValue = 'top';
+          topic = 'home';
+          break;
+        case "SportsHeadline":
+          suffix = 'Top Sports Story';
+          messAttrs.content.StringValue = 'sports';
+          topic = 'sports';
+          break;
+        case "PoliticsHeadline":
+          suffix = 'Top Politics Story';
+          messAttrs.content.StringValue = 'politics';
+          topic = 'politics';
+          break;
+        case "WorldHeadline":
+          suffix = 'Top World Story';
+          messAttrs.content.StringValue = 'world';
+          topic = 'world';
+          break;
+        default:
+          throw "unspecified intent";
+      }
+      writeToSqs(appName + '::' + suffix, messAttrs);
+      requestHeadline(context, topic);
     }
+  } catch (e) {
+    context.fail("Exception: " + e);
+  }
 };
 
 function requestHeadline(context, topic) {
